2a3c4067a3482caa24e513552ffdf6d074e76f0f
at0misk <ktp925@gmail.com>
2018-03-28 10:00:13 -0700

    Fe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 		@user = User.new(team: params['team'], first: params['first'], last: params['last'], email: params['email'], phone: params['phone'], tournament_type: params['tournament_type'], agree: true)
 		respond_to do |format|
 			if @user.save
-				UserMailer.reg_email(@user).deliver_now
+				# UserMailer.reg_email(@user).deliver_now
 				format.json do 
 					render json: @user
 				end
